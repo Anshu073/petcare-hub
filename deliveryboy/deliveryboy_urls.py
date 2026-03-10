018b2873139b6c5422a6f32200c0c2e8e5349e00
vraj537 <vraj537github@gmail.com>
2026-03-10 15:17:27 +0530

    edit_profile customer nu and deliveryboy nu merge thatu hatu ene solve kariyu

--- deliveryboy/deliveryboy_urls.py
+++ deliveryboy/deliveryboy_urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('login/', deliveryboy_views.delivery_login, name='delivery_login'),
     path('logout/', deliveryboy_views.delivery_logout, name='delivery_logout'),
     path('dashboard/', deliveryboy_views.delivery_dashboard, name='delivery_dashboard'),
-    path('edit-profile/', deliveryboy_views.edit_profile, name='edit_profile'),
+    path('edit-profile/', deliveryboy_views.edit_profile, name='edit__profile'),
     path('toggle-status/', deliveryboy_views.toggle_status, name='toggle_status'),
     path('deliver-order/<int:order_id>/', deliveryboy_views.deliver_order, name='deliver_order'),
 ]
