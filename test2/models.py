35b6cfa67a7e5888dbe2a855ce0a975f23cb8867
Ansh Prajapati <ansh07github@gmail.com>
2026-09-26 14:30:16 +0530

    fix area_name max_length

--- test2/models.py
+++ test2/models.py
@@ -11,7 +11,7 @@ name_regex = RegexValidator(regex=r'^[a-zA-Z\s]*$', message="Name can only conta
 #1
 class Area(models.Model):
     area_id = models.AutoField(primary_key=True)
-    area_name = models.CharField(max_length=10)
+    area_name = models.CharField(max_length=50)
     pincode = models.IntegerField()
 
     class Meta:
